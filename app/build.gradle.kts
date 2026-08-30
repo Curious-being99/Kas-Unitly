78267a5602e679002e5cd8e3ab1af7ccc5db0fb8
Curious-being99 <256735887+Curious-being99@users.noreply.github.com>
2026-08-29 23:21:21 -0400

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
       val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
       storeFile = file(keystorePath)
       storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
+      val alias = System.getenv("KEY_ALIAS")
+      keyAlias = if (alias.isNullOrEmpty()) "mykey" else alias
       keyPassword = System.getenv("KEY_PASSWORD")
     }
     create("debugConfig") {
